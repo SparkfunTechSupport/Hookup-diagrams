756fa350e269d3a8ca60521694d18cd22a413ed9
cmfrancis <christopher.francis@sparkfun.com>
2019-01-22 16:29:15 -0700

    Add files via upload
    Sketch to use with the hookup diagram.

---
+++ SEN-13030/Serial_passthrough_9600_baud_pins_2_and_3.ino
@@ -0,0 +1,41 @@
+/*****************************************************************
+Serial_Passthrough.ino
+
+Set up a software serial port to pass data between another serial 
+device and the serial monitor.
+
+Hardware Hookup:
+
+Connect your serial devices TX pin to Arduino pin D2 (Software RX)
+Connect your serial devices RX pin to Arduino pin D3 (Software TX)
+Connect ground on your serial device to ground on the Arduino
+*****************************************************************/
+
+// We'll use SoftwareSerial to communicate with the serial device.
+#include <SoftwareSerial.h>
+// Devices DOUT (TX) is connected to pin 2 (Arduino's Software RX)
+// Devices DIN (RX) is connected to pin 3 (Arduino's Software TX)
+SoftwareSerial Device(2,3); // RX, TX
+
+void setup()
+{
+  // Set up both ports at 9600 baud. This value is most important
+  // for the XBee. Make sure the baud rate matches the config
+  // setting of your XBee.
+  Device.begin(9600);     //Create a new 'software' UART on the Arduino and name it 'Device'
+  Serial.begin(9600);     //Connect the Arduino to the serial monitor over USB.
+  
+}
+
+void loop()
+{
+  if (Serial.available())
+  { // If data comes in from serial monitor, send it out to XBee
+    Device.write(Serial.read());
+  }
+  if (Device.available())
+  { // If data comes in from XBee, send it out to serial monitor
+    Serial.write(Device.read());
+  }
+}
+
